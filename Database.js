715295ca5c971fea84810aec66fc7d3d32bf4197
0pahh <Thierry.Angel@protonmail.com>
2022-12-19 17:53:17 +0100

    Querying the database

---
+++ Database.js
@@ -0,0 +1,7 @@
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+import { SUPABASE_URL, SUPABASE_KEY } from "@env";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase;
