2f8761f7dafc034f4a1f559b2b3474c0b3fb90c9
Lxuxs <louis.garrido42@gmail.com>
2022-12-19 17:20:38 +0100

    Tab navigation, Basis of screens and components

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SUPABASE_KEY } from "@env";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './pages/HomeScreen'
+import LoginScreen from './pages/LoginScreen'
+import SettingsScreen from './pages/SettingsScreen'
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Login" component={LoginScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
